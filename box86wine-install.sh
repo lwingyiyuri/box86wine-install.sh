1c7310e05c3cebdcb1f9a462dae38cb45dfafb63
lwingyiyuri <114618932+lwingyiyuri@users.noreply.github.com>
2022-10-08 21:57:29 +0630

    Create box86wine-install.sh

---
+++ box86wine-install.sh
@@ -0,0 +1,124 @@
+apt install ruby -y
+
+gem install lolcat 
+
+clear
+
+echo Waiting By ❤LwinGyi && YuRi❤   | lolcat -a -d 500
+
+clear
+
+function F1()
+
+{
+
+echo 'Welcome To LwinGyi&&YuRi | WinBox86 install Script'
+
+}
+
+F1
+
+echo -e "\nI LOVE \t YOU \t YURI\n"
+
+echo -e "\e[92m╔══════════════════════════════════════════╗"
+
+echo -e "\e[92m║██████╗  ██████╗ ██╗  ██╗ █████╗  ██████╗ ║"
+
+echo -e "\e[92m║██╔══██╗██╔═══██╗╚██╗██╔╝██╔══██╗██╔════╝ ║"
+
+echo -e "\e[92m║██████╔╝██║   ██║ ╚███╔╝ ╚█████╔╝███████╗ ║"
+
+echo -e "\e[92m║██╔══██╗██║   ██║ ██╔██╗ ██╔══██╗██╔═══██╗║"
+
+echo -e "\e[92m║██████╔╝╚██████╔╝██╔╝ ██╗╚█████╔╝╚██████╔╝║"
+
+echo -e "\e[92m║╚═════╝  ╚═════╝ ╚═╝  ╚═╝ ╚════╝  ╚═════╝ ║"
+
+echo -e "\e[92m╚════Box86 Inatall By LwinGyi And YuRi═════╝"
+
+sleep 9
+
+clear
+
+echo -e "\e[96m╔═════════════════════════════════════════════════════════════════════════════════════════╗  "
+
+echo -e "\e[96m║ ██╗     ██╗    ██╗██╗███╗   ██╗ ██████╗ ██╗   ██╗██╗   ♡   ██╗   ██╗██╗   ██╗██████╗ ██╗♡ "
+
+echo -e "\e[96m║ ██║     ██║    ██║██║████╗  ██║██╔════╝ ╚██╗ ██╔╝██║   ♡   ╚██╗ ██╔╝██║   ██║██╔══██╗██║♡  "
+
+echo -e "\e[96m║ ██║     ██║ █╗ ██║██║██╔██╗ ██║██║  ███╗ ╚████╔╝ ██║   ♡    ╚████╔╝ ██║   ██║██████╔╝██║♡ "
+
+echo -e "\e[96m║ ██║     ██║███╗██║██║██║╚██╗██║██║   ██║  ╚██╔╝  ██║   ♡     ╚██╔╝  ██║   ██║██╔══██╗██║♡  "
+
+echo -e "\e[96m║ ███████╗╚███╔███╔╝██║██║ ╚████║╚██████╔╝   ██║   ██║   ♡      ██║   ╚██████╔╝██║  ██║██║♡ "
+
+echo -e "\e[96m║ ╚══════╝ ╚══╝╚══╝ ╚═╝╚═╝  ╚═══╝ ╚═════╝    ╚═╝   ╚═╝   ♡      ╚═╝    ╚═════╝ ╚═╝  ╚═╝╚═╝♡  "
+
+echo -e "\e[96m╚═══════════One day I will marry Yuri and realize my dreams just like Yuri═══════════════╝  "
+
+sleep 15
+
+echo " Start Box86Install For 32bit Only Support" 
+
+echo " Wait For 5 Second "
+
+echo "1"
+
+sleep 1
+
+echo "2"
+
+sleep 2
+
+echo "3"
+
+sleep 3
+
+echo "4"
+
+sleep 4
+
+echo "5"
+
+sleep 5
+
+sudo apt update ; sudo apt install python3 gcc build-essential cmake git wget -y
+
+cd ~ ; git clone https://github.com/ptitSeb/box86 ; cd box86
+
+mkdir build; cd build; cmake .. -DCMAKE_BUILD_TYPE=RelWithDebInfo ; make ; sudo make install
+
+cd 
+
+mkdir ~/wine ; wget https://www.playonlinux.com/wine/binaries/phoenicis/upstream-linux-x86/PlayOnLinux-wine-7.0-rc5-upstream-linux-x86.tar.gz ; tar -xvf  PlayOnLinux-wine-7.0-rc5-upstream-linux-x86.tar.gz -C ~/wine/ ; echo "export BOX86_PATH=~/wine/bin/
+
+export BOX86_LD_LIBRARY_PATH=~/wine/lib/
+
+export BOX86_LOG=1" >> ~/.bashrc
+
+echo -e " \e[92m Thank For Use This Script"
+
+sleep 3
+
+echo -e "\e[92m Done Install For WineBox86 Install"
+
+sleep 2
+
+echo -e  "\e[92m Create By LwinGyi && YuRi"
+
+sleep 4
+
+echo -e  "\e[92m I want to say something"
+
+sleep 5
+
+echo -e "\e[92m I love Yuri forever"
+
+sleep 2
+
+echo -e "\e[92m I'm going to get married with Yuri one day"
+
+sleep 7
+
+echo -e "\e[92m Good Luck For A technician"
+
